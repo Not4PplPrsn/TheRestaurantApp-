dbbd945d377788b10f1c73cab69abb6d30d44c20
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-07 17:02:04 +0200

    Added some new changes added new dependencies. Added new files and added some new import for all the other files. Renamed some files. Deleted certain files in the folder. Added new images i will use.

--- app/EditPages/Add.tsx
+++ app/EditPages/Add.tsx
@@ -1,2 +1,4 @@
 import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { ImageBackground } from 'expo-image';
