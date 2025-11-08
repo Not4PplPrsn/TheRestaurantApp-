2338858b91c9c2becb4495339bd03ac680d0715f
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-08 16:16:12 +0200

    Added all the functions like the removal and calculating of the totals

--- functions/DishesEntries.ts
+++ functions/DishesEntries.ts
@@ -1,31 +1,37 @@
-import {create} from 'zustand'
-import { dishes } from './theDishArray'; // the other array so that they can be affect by the use of dishes 
+import { create } from 'zustand';
+import { dishes } from './theDishArray';
 
-
-   export type DishEntries = {
-    id: string;
-    dishName: string;
-    courseName: string;
-    description: string;
-    price: number;
-    image : any;
-     
- }
-
-type DishStore = {
-  entries: DishEntries[];
-  addDishes: (entry: DishEntries) => void;
-  removeDishes: (id: string) => void;
+export type DishEntries = { // The kinds of the information that can fill the array elements
+  id: string;
+  dishName: string;
+  courseName: string;
+  description: string;
+  price: number;
+  image: any;
 };
 
-export const useDishStore = create<DishStore>((set) => ({
-  entries: dishes, // ✅ initialize with static array
+type DishStore = {
+  entries: DishEntries[];//the package for the entries 
+  addDishes: (entry: DishEntries) => void; // sub function being set to an empty state
+  removeDishes: (id: string) => void; // sub function being set to an empty state 
+  getTotal: () => number; // total function set to an empty state
+};
+
+export const useDishStore = create<DishStore>((set, get) => ({
+  entries: dishes,
+
   addDishes: (entry) =>
-    set((state) => ({
+    set((state) => ({ // adds the information to the array
       entries: [...state.entries, entry],
     })),
+
   removeDishes: (id) =>
-    set((state) => ({
+    set((state) => ({ // will find the item with id and delete all other attributes with id attribute
       entries: state.entries.filter((dish) => dish.id !== id),
     })),
+
+  getTotal: () => {
+    const { entries } = get(); // This will add the prices of the selection
+    return entries.reduce((acc, dish) => acc + dish.price, 0);
+  },
 }));
