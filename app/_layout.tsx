f11ff9ef9e5c64d942d3aa85b7fea698657e3745
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-08 14:05:16 +0200

    Added new drawer thing for the information in the drawer. Added another drawer function.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import { useRoute } from '@react-navigation/native';
+import { Drawer } from 'expo-router/drawer';
+import React from 'react';
 import 'react-native-reanimated';
 
 
@@ -14,12 +15,45 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Drawer
+    screenOptions={{
+      drawerType: 'front', headerShown: true/**Force the header for the drawer to show  */,
+            drawerStyle: { width: 350,
+        direction: 'ltr',
+        paddingTop: 5,
+        paddingBottom: 15,
+        justifyContent: 'space-between',
+        borderTopRightRadius: 10,
+        animationDirection: 'reverse',
+        animationName:'slide-in-right',
+        animationFillMode: 'forwards',
+        animationDuration: '5050ms',
+      },
+            
+      drawerPosition: 'right',
+      drawerLabelStyle: { textAlign: 'right', paddingRight: 10, paddingLeft: 0 ,},
+      headerTitleAlign: 'left', 
+
+
+    }}
+    />
+  );
+}
+
+function CustomDrawerContent(){
+  const router = useRoute; // this is what we will use fo the exclusion of certain pages
+
+
+  return(
+    <DrawerContentScrollView>
+      <DrawerItemList>
+        <DrawerItem/>
+        <DrawerItem/>
+        <DrawerItem/>
+        <DrawerItem/>
+
+      </DrawerItemList>
+    </DrawerContentScrollView>
+
   );
 }
