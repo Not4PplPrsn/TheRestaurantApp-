fcd234515cd5318a998feb372663b1b511b92987
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-09 13:13:01 +0200

    Added a filter function and edited the addDishes function to clear the input sections when adding the item ot the array.

--- functions/DishesEntries.ts
+++ functions/DishesEntries.ts
@@ -13,17 +13,19 @@ export type DishEntries = { // The kinds of the information that can fill the ar
 type DishStore = {
   entries: DishEntries[];//the package for the entries 
   addDishes: (entry: DishEntries) => void; // sub function being set to an empty state
-  removeDishes: (id: string) => void; // sub function being set to an empty state 
-  getTotal: () => number; // total function set to an empty state
+  removeDishes: (id: string) => void; // sub function being set to an empt
+  getByCourse: (courseName: string) => void; // setting the filtering empty filtering
 };
 
 export const useDishStore = create<DishStore>((set, get) => ({
   entries: dishes,
+   addDishes:(entry) => {
+    const alreadyExists = dishes.some((dish) => dish.dishName === entry.dishName);/**This will check the array for items for item with the same attributes */
+    const updated = alreadyExists ? dishes : [...dishes, entry];/**Reset the the array back to its original form  */
+    return {entries:updated}/**Will return the new array */
 
-  addDishes: (entry) =>
-    set((state) => ({ // adds the information to the array
-      entries: [...state.entries, entry],
-    })),
+  
+  },
 
   removeDishes: (id) =>
     set((state) => ({ // will find the item with id and delete all other attributes with id attribute
@@ -34,4 +36,8 @@ export const useDishStore = create<DishStore>((set, get) => ({
     const { entries } = get(); // This will add the prices of the selection
     return entries.reduce((acc, dish) => acc + dish.price, 0);
   },
+  getByCourse: (course: string) => { // this will filter the dishes based on the course name
+  const { entries } = get();
+  return entries.filter((dish) => dish.courseName === course);
+}
 }));
