56b7a48e9fe5b1012b92d3f740ff7a42386e8484
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-12 18:24:45 +0200

    Added new icons and new error handling for the fillable items. For  the description has a word limit so as not to be long winded, the price has ot be a number and all items have to be filled in order for the add button to work.  The description text box was styled and the view for the whole form is also styled such that it can look nice. Added new styles for the purpose in the style sheet.

--- app/EditPages/Add.tsx
+++ app/EditPages/Add.tsx
@@ -8,19 +8,70 @@ import React,{useState} from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import { useDishStore } from '@/functions/DishesEntries';// calling the function 
 import { RadioButton } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
+import { Entypo, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { DishEntries } from '@/functions/DishesEntries';
+import { Alert } from 'react-native';
+
+
+
 const router = useRouter(); // function for navigation
 
 
 export default function Added(){
 
-  const addEntries = useDishStore((state) => state.addDishes);
+  const addEntries = useDishStore((state) => state.addDishes) ;
+
+  const [newPrice,setNewPrice] = useState(0);// prices will be stored here
     
-  const [checked, setChecked] = useState();//these are meant to store the responses 
+  const [description, setDescription] = useState(''); // will stores th written description,
+
+  const [dishName, setDishName] = useState('')//the dish name will be stored here 
 
   const [selectedCourse, setSelectedCourse] = useState('Entrée');//standardized the radio buttons 
 
   const [selectedImage, setSelectedImage] = useState<any>(null);// set image picker to an empty state
+
+const handleAdd = () => {
+  // storing the trimmed versions of the things in new variables so that they are easier to handle
+  const trimmedDishName = dishName.trim();
+  const trimmedDescription = description.trim();
+  const trimmedCourse = selectedCourse.trim();
+  const wordCount = trimmedDescription.split(/\s+/).length;// this is meant to sore the ammount of words here based on how many characters are grouped for new 
+
+if (!trimmedDescription || !trimmedDishName || !selectedImage || !trimmedCourse || newPrice <= 0) {
+  Alert.alert('Please enter all fields and ensure the price is valid.'); // error handling of 
+  return;
+}
+if (wordCount < 50 || wordCount > 70) {
+  Alert.alert('Description must be between 50 and 70 words.');//word limit checking so  that the description is not to long
+  return;
+}
+  if (newPrice <= 0 || typeof newPrice !== 'number' ) {
+  console.warn('Please enter a valid price.');
+  return;
+}
+
+
+
+  const newDish: DishEntries = {
+    id: 0, // started the id on zero
+    dishName: trimmedDishName, 
+    courseName: trimmedCourse,
+    description: trimmedDescription,
+    price: newPrice, // default price
+    image: selectedImage, // URI string
+    isDeleted: false,
+  };
+
+  addEntries(newDish);
+
+  // Reset form fields
+  setDescription('');
+  setSelectedCourse('Entrée'); // made entree th default so the user can start
+  setSelectedImage(null);
+  setDishName('');
+  setNewPrice(0)
+};
 
     const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -36,6 +87,8 @@ export default function Added(){
 
 
     return(
+
+    
   <ImageBackground
   source={require('../../assets/images/Backgrounds/TableSetting4.jpg')}
         style={[StyleSheet.absoluteFill,{opacity:20} ]}
@@ -76,25 +129,58 @@ export default function Added(){
           
 
           <Text style={styles.heading}>Editing</Text>
+  <View style = {styles.FormContainer}>
     
-    <View>
-      <Button title="Select an Image" onPress={pickImage} />
+  <View style={{margin: 25.4,   }}> 
+
+
+
+  <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+    <TouchableOpacity
+    onPress={pickImage} >
+    <View style={styles.imagePickerButton}>
+      <Text>Add Image</Text>
     </View>
-  <View>        
-{selectedImage && (
-    <Image source={{ uri: selectedImage }}/>)}
+    </TouchableOpacity>
+
+      <Image
+      style = {styles.pickedImageDisplay}
+      source={{uri: selectedImage}}
+      />
+
+</View >
+
+          <View>
+              <TextInput
+              placeholder='Enter the Price'
+             onChangeText={(text) => setNewPrice(Number(text))}
+              keyboardType='numeric'
+              value={newPrice.toString()}
+              />
+          </View>
 
 
                 
-                <TextInput placeholder='Add Dish name'/>
-              <TextInput placeholder='Write a description that is at least 50 words long.'/>
-
+                <TextInput placeholder='Add Dish name'
+                value={dishName}
+                onChangeText={setDishName}
+                                
+                />
+<TextInput
+  placeholder='Write a description that is at least 50 words long.'
+  value={description}
+  onChangeText={setDescription}
+  multiline
+  numberOfLines={5}
+style={styles.theDescription}
+/>
 
               {/**The radio button for the courses */}
-              <View>
+              <View style = {{ flexDirection: 'row', height: 60, padding: 12}}>
                 <RadioButton.Group
                 onValueChange={(newValue) => setSelectedCourse(newValue)}
                 value={selectedCourse}
+                
                 
                 >
                   <RadioButton.Item label='Entrée' value='Entrée' labelStyle= {{margin: 1}}/>
@@ -103,7 +189,17 @@ export default function Added(){
               
                 </RadioButton.Group>
               </View>
-</View>
+</View>      
+</View> 
+        <View style={{flexBasis: 'auto', alignSelf: 'center', margin: 30}}>
+                <TouchableOpacity
+                onPress={handleAdd}
+                >
+                  <View>
+                    <MaterialIcons name="add-circle" size={55} color={'#aaf095ff'}/>
+                  </View>
+                </TouchableOpacity>
+        </View>
       
       </ImageBackground>
     );
@@ -172,7 +268,51 @@ buttonText: {
   margin: 5,
   fontWeight:'700',
   fontFamily: 'Roboto'
-}
+}, 
+description: {
+  width: 250,
+  height: 150
+},
 
+imagePickerButton: {
+  width: 95,
+  height: 40,
+  backgroundColor: '#d6c2c2ff',
+  alignSelf: 'center', 
+  borderRadius : 6,
+  marginStart: 10,
+  alignItems: 'center',
+  padding: 10, 
+}, 
+pickedImageDisplay: {
+  width : 140,
+  height: 100,
+  borderRadius: 7,
+  
+},
+
+FormContainer: {
+  height: 490, 
+  width: 330, 
+  padding: 10, 
+  backgroundColor: '#f3b25ca9',
+  borderBottomRightRadius: 20.,
+  borderTopLeftRadius: 20,
+  margin: 15, 
+  alignSelf: 'center'
+},
+theDescription:{
+  width: 250,
+  height: 70,
+  backgroundColor: '#c52b1d8c',
+  borderRadius: 12,
+  fontSize: 10
+},
+radioButtonGroup:{
+  fontSize: 15,
+  fontStyle: 'italic',
+  fontFamily: 'monospace',
+  alignItems: 'flex-start'
+}
 
 });
