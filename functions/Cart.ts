9d09bb839314f0faedb776eae7ec4a98eb532220
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-12-02 13:20:46 +0200

    transferred the files fo a new repo in the local machine

--- functions/Cart.ts
+++
@@ -1,5 +0,0 @@
-import { DishEntries } from "./DishesEntries";
- 
-type Choice = DishEntries; // type definition for the array /**(W3Schools, 2025) */
-
-export const Cart: Choice[] /**(W3Schools, 2025) */ = []/**The empty array for the cart */
--- functions/DeletedDishes.ts
+++
@@ -1,8 +0,0 @@
-// this is where the deleted items will be stored 
-import { DishEntries } from "./DishesEntries";
-
-
-//stored the exportable type in  a new type container
-type Dish = DishEntries; /**(W3Schools, 2025) */
-
-export const Trash: Dish[] = [];
